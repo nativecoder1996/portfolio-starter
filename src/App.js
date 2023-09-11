a67d0e1fdf19a6366795292c3a0bd4c7ddd1af58
nativecoder1996 <gitHubcopilot1@gmail.com>
2023-09-11 15:43:51 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import Navbar from "./Component/NavBar/Navbar";
+import Intro from "./Component/Intro/Intro";
+import './App.css';
+import Services from "./Component/Services/Services";
+
 function App() {
   return (
     <div className="App">
-      Subscribe ZainKeepsCode
+     <Navbar/>
+      <Intro/>
+      <Services/>
     </div>
   );
 }
